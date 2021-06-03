93c5822c5df1b5ac57b9a4711c67d06c4c24eb20
Admin <jontoshmatov@yahoo.com>
2021-06-02 20:21:29 -0400

    I am pushing my new code for lessons projects

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<h2>Hello World!</h2>
+<hr>
+<img alt="This is where you put the img desc" class="img_new_yor_city" src="https://media.tacdn.com/media/attractions-content--1x-1/0b/2d/dc/c0.jpg">
+</body>
+</html>
