c890a83ba80e13495b1f44a9281aa104e0cd5dc7
Jon Toshmatov <jontoshmatov@jons-mbp.myfiosgateway.com>
2021-06-02 20:50:27 -0400

    I completed the ticket number 5 and verified and QAed by JT

--- index.php
+++ index.php
@@ -11,6 +11,18 @@
 <body>
 <h2>Hello World!</h2>
 <hr>
-<img alt="This is where you put the img desc" class="img_new_yor_city" src="https://media.tacdn.com/media/attractions-content--1x-1/0b/2d/dc/c0.jpg">
+<h3>Our Promo Video</h3>
+<iframe width="70%" height="450px" src="https://www.youtube.com/embed/ZcaiYDtR0PM?controls=0" title="YouTube video player"
+        frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>
+<hr>
+<img alt="This is where you put the img desc" class="img_new_yor_city"
+     src="https://media.tacdn.com/media/attractions-content--1x-1/0b/2d/dc/c0.jpg">
+<h3>Contact Us</h3>
+<hr>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3028.9955657537607!2d-73.9417760845982!3d40.607921479343354!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c2449efd447f43%3A0xd9165aef3346c7a5!2s1868%20Burnett%20St%2C%20Brooklyn%2C%20NY%2011229!5e0!3m2!1sen!2sus!4v1622681142102!5m2!1sen!2sus"
+        width="90%" height="550px" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+<br>
+<hr>
 </body>
 </html>
